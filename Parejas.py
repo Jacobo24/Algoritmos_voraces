5906cb1eaebfc53ea9096c08bbd0552b48a92b54
Jacobo24 <jaccalgar@gmail.com>
2023-11-26 22:15:16 +0100

    fin

--- Parejas.py
+++ Parejas.py
@@ -1,12 +1,12 @@
 def min_swaps_to_adjacent(row):
     swaps = 0
-    position = {x: i for i, x in enumerate(row)}
+    posicion = {x: i for i, x in enumerate(row)}
     for i in range(0, len(row), 2):
-        expected_partner = row[i] + 1 if row[i] % 2 == 0 else row[i] - 1
-        partner_index = position[expected_partner]
-        if partner_index != i + 1:
-            row[i + 1], row[partner_index] = row[partner_index], row[i + 1]
-            position[row[partner_index]] = partner_index
-            position[row[i + 1]] = i + 1
+        expected_pareja = row[i] + 1 if row[i] % 2 == 0 else row[i] - 1
+        pareja_index = posicion[expected_pareja]
+        if pareja_index != i + 1:
+            row[i + 1], row[pareja_index] = row[pareja_index], row[i + 1]
+            posicion[row[pareja_index]] = pareja_index
+            posicion[row[i + 1]] = i + 1
             swaps += 1
     return swaps
